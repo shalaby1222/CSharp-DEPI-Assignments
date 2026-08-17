65d1d4ed626da9820ce1a4eb2fb1766498fd2a4e
mohamed reda shalaby <146749389+shalaby1222@users.noreply.github.com>
2026-08-17 16:12:46 +0300

    Add files via upload

---
+++ Session-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DEPI_Ass1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
